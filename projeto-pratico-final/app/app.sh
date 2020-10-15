4ca502560b581c56b7972cf840b2d5e6a642026b
Paulo Ricardo <pricardo.ti@gmail.com>
2020-10-15 08:48:35 -0300

    Atualizando projeto.

--- projeto-pratico-final/app/app.sh
+++ projeto-pratico-final/app/app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-pip install bottle==0.12.13 redis==2.10.5 #psycopg2==2.7.1
+pip install bottle==0.12.13 redis==2.10.5 psycopg2==2.7.1
 python -u sender.py
--- projeto-pratico-final/app/sender.py
+++ projeto-pratico-final/app/sender.py
@@ -1,23 +1,33 @@
-# import psycopg2
+import psycopg2
 import redis
 import json
+import os
 
 from bottle import Bottle, request
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+
+        dsn = f'dbname={db_name} user={db_user} host={db_host}'
+        print('>>> Sender :: dsn[', dsn, ']')
+
         self.route('/', method='POST', callback=self.send)
         self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
 #         DSN = 'dbname=email_sender user=postgres host=db'
-#         self.conn = psycopg2.connect(DSN)
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
-#         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
-#         cur = self.conn.cursor()
-#         cur.execute(SQL, (assunto, mensagem))
-#         self.conn.commit()
-#         cur.close()
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
         msg = {'assunto': assunto, 'mensagem': mensagem}
         self.fila.rpush('sender', json.dumps(msg))
         print('Mensagem registrada !')
